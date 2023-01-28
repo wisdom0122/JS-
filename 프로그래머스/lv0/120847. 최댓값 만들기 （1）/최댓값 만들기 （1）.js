1593639ae96934ff6d85a999bfb59a1fee309832
JIHYE LEE <115059778+wisdom0122@users.noreply.github.com>
2023-01-28 15:38:52 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -1,7 +1,4 @@
 function solution(numbers) {
-    var answer = numbers.sort(function(a,b){
-        return a-b});
-    var answer = answer.reverse();
-    var a = answer[0]*answer[1]
-    return a;
+    var answer = numbers.sort((a,b)=>b-a);
+    return answer[0]*answer[1];
 }
